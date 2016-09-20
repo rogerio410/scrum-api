84c2b0f72d7fa1425fb50dec40b7eb0817aedfbf
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2016-09-20 11:45:13 -0300

    Serializers for Sprint and Mixin por Configs

--- board/views.py
+++ board/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, authentication, permissions
 
-# Create your views here.
+from .models import Sprint
+from .serializers import SprintSerializer
+
+
+class DefaultsMixin(object):
+    """Default settings for view authentication, permissions, filtering and pagination."""
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating sprints."""
+
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
