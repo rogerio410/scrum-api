b5b3f7d3118030c1ab937898c7c835d47bea8b14
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2016-09-20 23:45:08 -0300

    Inclusão de Buscar, Ordenação e Paginação.

--- board/views.py
+++ board/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import viewsets, authentication, permissions
+from rest_framework import viewsets, authentication, permissions, filters
 
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
@@ -22,12 +22,19 @@ class DefaultsMixin(object):
     paginate_by = 25
     paginate_by_param = 'page_size'
     max_paginate_by = 100
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
 
 class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """API endpoint for listing and creating sprints."""
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+    search_fields = ('name',)
+    ordering_fields = ('end', 'name',)
 
 
 class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
@@ -35,6 +42,8 @@ class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    search_fields = ('name', 'description',)
+    ordering_fields = ('name', 'order', 'started', 'due', 'completed',)
 
 
 class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
@@ -44,3 +53,4 @@ class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
     lookup_url_kwarg = User.USERNAME_FIELD
     queryset = User.objects.order_by(User.USERNAME_FIELD)
     serializer_class = UserSerializer
+    search_fields = (User.USERNAME_FIELD,)
--- scrum_api/settings.py
+++ scrum_api/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
