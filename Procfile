ba1e9aa4e3a9eee228852c7e2bb9aa6dd5d587f4
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2016-09-21 12:05:23 -0300

    Heroku Deploy Config.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn scrum_api.wsgi --log-file -
--- scrum_api/settings.py
+++ scrum_api/settings.py
@@ -123,3 +123,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
 }
+
+if not DEBUG:
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
